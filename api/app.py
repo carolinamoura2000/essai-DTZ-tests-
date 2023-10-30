28ad090da6e4f933fab48c142842203cee831376
carolinamoura2000 <124861481+carolinamoura2000@users.noreply.github.com>
2023-10-30 16:25:57 -0300

    step1: add calculator backend and tests

---
+++ api/app.py
@@ -0,0 +1,44 @@
+from flask import (
+    Flask,
+    request,
+)
+
+from calculator.calculator import Calculator
+
+app = Flask(__name__)
+
+#3@app.route('/')
+#def home():
+#    return 'Hello, World!'
+
+@app.route('/')
+def home():
+    return 'Welcome to the Calculator API'
+
+
+@app.route('/api/add', methods=['POST'])
+def add():
+    return operation('add', 2)
+
+@app.route('/api/subtract', methods=['POST'])
+def subtract():
+    return operation('subtract', 2)
+
+@app.route('/api/multiply', methods=['POST'])
+def multiply():
+    return operation('multiply', 2)
+
+@app.route('/api/divide', methods=['POST'])
+def divide():
+    return operation('divide', 2)
+
+def operation(method, num_factors):
+    factors = []
+    if num_factors == 2:
+        factors.append(float(request.json.get('x')))
+        factors.append(float(request.json.get('y')))
+
+    return str(getattr(Calculator, method)(*factors))
+
+
+app.run(host='0.0.0.0', port=8080)
